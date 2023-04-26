6f135946c8b9d1310add0d4615c0c43adba15575
Vítor F. Nery <vitorfelipenery@gmail.com>
2023-04-26 09:21:59 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Modal from "./components/Modal";
+import { Trash } from "@phosphor-icons/react";
+
+function App() {
+  const [open, setOpen] = useState(false);
+  return (
+    <main className="App">
+      <button className="btn btn-danger" onClick={() => setOpen(true)}>
+        <Trash /> Delete
+      </button>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Trash size={56} className="mx-auto text-red-500" />
+        <div className="mx-auto my-4 w-48">
+          <h3 className="text-lg font-black text-gray-800">Confirm Delete</h3>
+          <p className="text-sm text-gray-500">
+            Are you sure you want to delete this item?
+          </p>
+        </div>
+        <div className="flex gap-4">
+          <button className="btn btn-danger w-full">Delete</button>
+          <button
+            className="btn btn-light w-full"
+            onClick={() => setOpen(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      </Modal>
+    </main>
+  );
+}
+
+export default App;
